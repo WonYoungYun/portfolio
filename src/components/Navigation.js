0ee906ef29cccf4d9726fd9d776a39db0029b729
WonYoungYun <asmar20288@naver.com>
2019-01-03 19:59:26 +0900

    navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,7 +1,45 @@
 import React, { Component } from "react";
-
+import "./Navigation.css";
 export default class Navigation extends Component {
+  state = {
+    classList: ""
+  };
+  componentDidMount = () => {
+    window.addEventListener("scroll", this.handleScroll);
+  };
+  componentWillUnmount = () => {
+    window.removeEventListener("scroll", this.handleScroll);
+  };
+  handleScroll = e => {
+    let windowY = window.scrollY;
+    if (windowY !== 0) {
+      this.setState({
+        classList: "scroll"
+      });
+    } else {
+      this.setState({
+        classList: ""
+      });
+    }
+  };
   render() {
-    return <div id="nav">NAV</div>;
+    return (
+      <div id="nav" className={this.state.classList}>
+        <ul>
+          <li>
+            <a href="#slider">Home</a>
+          </li>
+          <li>
+            <a href="#aboutMe">About Me</a>
+          </li>
+          <li>
+            <a href="#project">Project</a>
+          </li>
+          <li>
+            <a href="#contactMe">Contact Me</a>
+          </li>
+        </ul>
+      </div>
+    );
   }
 }
--- src/components/Project.js
+++ src/components/Project.js
@@ -2,6 +2,14 @@ import React, { Component } from "react";
 
 export default class Project extends Component {
   render() {
-    return <div id="project">Project</div>;
+    return (
+      <div id="project">
+        <ul>
+          <li>a</li>
+          <li>a</li>
+          <li>a</li>
+        </ul>
+      </div>
+    );
   }
 }
