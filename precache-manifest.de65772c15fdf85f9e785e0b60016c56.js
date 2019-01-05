ee5641b06e7201841c41cdeb426a2063c7204b8e
WonYoungYun <asmar20288@naver.com>
2019-01-05 23:46:06 +0900

    Updates

---
+++ precache-manifest.de65772c15fdf85f9e785e0b60016c56.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "99bfb99cf8f9f115664b02e11f72c9cf",
+    "url": "/portfolio/static/media/movieApp.99bfb99c.png"
+  },
+  {
+    "revision": "70b1d6af086f11a45de5",
+    "url": "/portfolio/static/css/main.5fe6886e.chunk.css"
+  },
+  {
+    "revision": "98d7e95f98f11b6bae51",
+    "url": "/portfolio/static/js/1.98d7e95f.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "2107ae9a05a32a23505ca6d58def05ee",
+    "url": "/portfolio/static/media/txtRPG.2107ae9a.png"
+  },
+  {
+    "revision": "ef9331a2f7851b14de2d330e667e634c",
+    "url": "/portfolio/static/media/todoList.ef9331a2.png"
+  },
+  {
+    "revision": "70b1d6af086f11a45de5",
+    "url": "/portfolio/static/js/main.70b1d6af.chunk.js"
+  },
+  {
+    "revision": "1a77bd739b1cc98320fae275d4b247e8",
+    "url": "/portfolio/static/media/board.1a77bd73.png"
+  },
+  {
+    "revision": "ed91b36eebfb1ee9dede02443a212112",
+    "url": "/portfolio/static/media/about.ed91b36e.jpg"
+  },
+  {
+    "revision": "4fc986929079179ec6035ddbf2a5694e",
+    "url": "/portfolio/static/media/project.4fc98692.jpg"
+  },
+  {
+    "revision": "34077c2ae4a023e25230a44ae5ecaa03",
+    "url": "/portfolio/static/media/contact.34077c2a.jpg"
+  },
+  {
+    "revision": "ebfc66b8126e1ffc9de4bcc343dd85a7",
+    "url": "/portfolio/index.html"
+  }
+];
